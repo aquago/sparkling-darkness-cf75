1a4544d3d864c274d0f0081c187a385d13d46dad
dmstaanacoding <132879578+dmstaanacoding@users.noreply.github.com>
2025-03-13 13:17:12 +0800

    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'], // Scan index.html for Tailwind classes
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/', // Base path for Cloudflare Pages
+  base: '/',
   build: {
-    outDir: 'dist' // Output directory
-  }
+    outDir: 'dist',
+  },
+  plugins: [
+    tailwindcss(),
+  ],
 });
