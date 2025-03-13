0fb81ed6f21c55813c7e003dfdfbd537a936022d
dmstaanacoding <132879578+dmstaanacoding@users.noreply.github.com>
2025-03-13 11:46:56 +0800

    Commit changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  base: '/', // Base path for Cloudflare Pages
+  build: {
+    outDir: 'dist' // Output directory
+  }
 });
